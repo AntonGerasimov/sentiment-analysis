137a80d832abd5855531b79376472c9128adca1e
Anton <Anton@localadms-MacBook-Pro.local>
2024-02-06 14:34:31 +0400

    Change path to settings json

--- src/data_loader.py
+++ src/data_loader.py
@@ -15,7 +15,7 @@ ROOT_DIR = os.path.dirname(CURRENT_DIR)
 
 sys.path.append(os.path.dirname(ROOT_DIR))
 
-CONF_FILE = "settings.json"
+CONF_FILE = os.path.join(ROOT_DIR, "settings.json")
 
 with open(CONF_FILE, "r") as file:
     conf = json.load(file)
